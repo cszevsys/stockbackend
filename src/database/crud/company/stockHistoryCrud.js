7d6383950fc9a8a8039e99c7ddff7c0e621ae8ce
cszevsys <chungsze.wong@v.systems>
2024-03-23 19:58:50 +0800

    desc sort

--- src/database/crud/company/stockHistoryCrud.js
+++ src/database/crud/company/stockHistoryCrud.js
@@ -2,12 +2,12 @@ import StockHistory from "../../schema/company/stockHistorySchema.js";
 
 export default{
     async getStockHistoryByName(companyName){
-        const history = await StockHistory.find({companyName: companyName}).sort({date: 1});
+        const history = await StockHistory.find({companyName: companyName}).sort({date: -1});
         return history;
     },
 
     async getStockHistoryByCode(stockCode){
-        const history = await StockHistory.find({stockCode: stockCode}).sort({date: 1});
+        const history = await StockHistory.find({stockCode: stockCode}).sort({date: -1});
         return history;
     }
 
